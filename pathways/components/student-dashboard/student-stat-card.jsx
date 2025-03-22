fb5dd576971d2738514901cc3faca870a534fd87
Avi Walia <78369627+Avi04w@users.noreply.github.com>
2025-03-22 02:04:25 -0400

    ui changes

--- pathways/components/student-dashboard/student-stat-card.jsx
+++ pathways/components/student-dashboard/student-stat-card.jsx
@@ -4,7 +4,7 @@ export default function StudentStatCard({ title, value, Icon, message }) {
       <div className="bg-white rounded-lg shadow-md p-8 flex flex-col justify-between">
         <div>
           <p className="text-black text-lg font-semibold">{title}</p>
-          <div className="flex flex-row justify-between mt-2 text-blue-500">
+          <div className="flex flex-row justify-between mt-2 text-amber-600">
             <p className="text-black text-3xl font-black">{value}</p>
             <Icon />
           </div>
