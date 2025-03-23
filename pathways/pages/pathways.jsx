390d29381b105024a43274caa4081bd153cd9450
Avi Walia <78369627+Avi04w@users.noreply.github.com>
2025-03-23 02:46:22 -0400

    make pathways button

--- pathways/pages/pathways.jsx
+++ pathways/pages/pathways.jsx
@@ -5,10 +5,12 @@ import PathwayCard from "@/components/pathways/pathways-card"
 import { useRouter } from "next/navigation"
 import CreateRoadmapModal from "@/components/modals/CreateRoadmapModal"
 import { useState } from "react"
+import backendUrl from "@/backendUrl"
 
 export default function Pathways() {
   const router = useRouter()
-  const [showModal, setShowModal] = useState(false)
+  const [showRoadmapModal, setShowRoadmapModal] = useState(false);
+  const user = JSON.parse(localStorage.getItem("user"));
   const mockPathways = [
     {
       id: 1,
@@ -42,9 +44,24 @@ export default function Pathways() {
     },
   ]
 
-  const handleCreateNewPathway = () => {
-    setShowModal(true);
+  const createRoadmap = async (data) => {
+      try {
+          const res = await fetch(`${backendUrl}/generate-roadmap/`, {
+              method: "POST",
+              headers: { "Content-Type": "application/json" },
+              body: JSON.stringify(data),
+          });
+
+          const ret = await res.json();
+          console.log("ret", ret);
+
+          return ret?.roadmap;
+      } catch (error){
+          console.error("Failed to create roadmap", error);
+      }
   }
+
+  console.log("user", user);
 
   return (
     <div className="flex flex-col bg-gray-100 min-h-screen">
@@ -63,7 +80,9 @@ export default function Pathways() {
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
             {/* Create Pathway Card FIRST */}
             <button
-              onClick={handleCreateNewPathway}
+              onClick={() => {
+                setShowRoadmapModal(true);
+            }}
               className="flex flex-col items-center justify-center border-2 border-dashed border-gray-300 rounded-lg p-4 text-center text-gray-500 hover:bg-amber-100 cursor-pointer transition"
             >
               <p className="text-sm md:text-base font-medium text-gray-600">
@@ -83,14 +102,11 @@ export default function Pathways() {
             ))}
           </div>
           <CreateRoadmapModal
-            isOpen={showModal}
-            onClose={() => setShowModal(false)}
-            onCreate={(newRoadmap) => {
-                console.log("Created roadmap:", newRoadmap);
-                // You can optionally update state here to show new pathways live
-                setShowModal(false);
-            }}
-            />
+              isOpen={showRoadmapModal}
+              onClose={() => setShowRoadmapModal(false)}
+              onCreate={async (data) => await createRoadmap(data)}
+              user={user}
+          />
         </div>
       </div>
     </div>
