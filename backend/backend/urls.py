6b5dddcf56ef333389541cc298ce9b4a7aa0ff84
dhruvpatt <84606623+dhruvpatt@users.noreply.github.com>
2025-03-23 01:58:01 -0400

    markdown on module

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -6,7 +6,7 @@ from pathways import views
 from pathways.roadmaps_views import RoadmapGenerationAPIView, get_all_roadmaps, get_roadmap_by_id, update_roadmap, \
     delete_roadmap, get_all_subjects, get_subject_by_id, create_subject, update_subject, delete_subject, \
     get_all_chapters, get_chapter_by_id, create_chapter, update_chapter, delete_chapter, get_user_roadmaps, publish_roadmap_to_classroom
-from pathways.module_gen import ModuleContentGenerationAPIView, ModuleAssistantAPIView, get_module, roadmap_progress
+from pathways.module_gen import ModuleContentGenerationAPIView, ModuleAssistantAPIView, get_module
 from pathways.quiz_gen import QuizGenerationAPIView, QuizEvaluationAPIView
 from pathways.classroom_views import classroom_analytics, classroom_student_details, join_classroom_as_student, \
     join_classroom_as_teacher, create_classroom, student_classroom_analytics
