1e9f52f5688669cf82d6ddf4f627d8e46b3aa42e
BradGardea <brad.gardea@gmail.com>
2025-06-19 13:57:22 -0400

    devtools

--- pathways/pages/_app.js
+++ pathways/pages/_app.js
@@ -2,6 +2,7 @@ import DashboardLayout from "../components/DashboardLayout";
 import "../styles/globals.css";
 import { useRouter } from "next/router";
 import { AuthProvider } from "@/contexts/useAuth";
+import ErudaDevTools from "@components/devtools";
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
@@ -13,6 +14,7 @@ export default function App({ Component, pageProps }) {
 
   return (
     <div className="min-h-screen w-full bg-animated-gradient">
+      <ErudaDevTools/>
       <AuthProvider>
         {shouldUseLayout ? <DashboardLayout>{Page}</DashboardLayout> : Page}
       </AuthProvider>
